771afc9af0b377d3058e5700a14a53cc167f1a04
LupusLudensEst <gurovvic@gmail.com>
2019-12-17 21:08:42 -0500

    upgrade dt 17 Dec 2019

--- features/steps/guest_explores_ports_of_departure.py
+++ features/steps/guest_explores_ports_of_departure.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 
 use_step_matcher("re")
 
-SH_EXC_IS_PRESENT = (By.CSS_SELECTOR, "h2.header-title.visible-desktops")
 
 @step('I navigated to "Shore Excursion" page')
 def navigated_shore_excursions(context):
@@ -21,20 +20,17 @@ def nav_excursions(context):
 
 @step("Shore Excursions page is present")
 def shore_exc_is_present(context):
-    #context.app.shore_excursions.text_is_present()
-    TEXT_IS_HERE = context.driver.find_element(*SH_EXC_IS_PRESENT).text
-    assert 'Shore Excursions' in TEXT_IS_HERE
-    print(f'Text: {TEXT_IS_HERE} .')
-    sleep(2)
+    context.app.shore_excursions.text_is_present()
 
 
 @when('Price range is filtered to "\$0-\$30"')
 def price_range_is(context):
-    context.driver.get('https://www.ncl.com/shore-excursions/search?sort=searchWeight&perPage=12&priceRange=0+30')
+    context.app.shore_excursions.price_range_present()
 
 
 @then("Only shore excursions within range are displayed")
 def no_more_thirty_is_here(context):
+    context.driver.get('https://www.ncl.com/shore-excursions/search?sort=searchWeight&perPage=12&priceRange=0+30')
     assert 'priceRange=0+30' in context.driver.current_url == 'https://www.ncl.com/shore-excursions/search?sort=searchWeight&perPage=12&priceRange=0+30'
 
 
--- pages/shore_excursions.py
+++ pages/shore_excursions.py
@@ -5,9 +5,18 @@ from selenium.webdriver.common.by import By
 class ShoreExcursions(Page):
     NAV_EXCURSIONS = (By.CSS_SELECTOR, "button.btn-cta.btn-primary.btn-large.search-submit")
     SH_EXC_IS_PRESENT = (By.CSS_SELECTOR, "h2.header-title.visible-desktops")
+    PRICE_RANGE_PRESENT = (By.CSS_SELECTOR, "span.legend-column.extremes")
 
     def click_find_excursions(self):
         self.wait_for_element_click(*self.NAV_EXCURSIONS)
 
     def text_is_present(self):
-        self.text_is_present(*self.SH_EXC_IS_PRESENT)
+        self.find_element(*self.SH_EXC_IS_PRESENT).text
+
+    def price_range_present(self):
+        self.click(*self.PRICE_RANGE_PRESENT)
+
+
+
+
+
